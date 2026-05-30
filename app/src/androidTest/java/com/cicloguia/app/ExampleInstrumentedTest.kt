4c31123db554f7a1c1eda3f4eb56991763012272
Hernan Pareja <hernan.pareja@globant.com>
2026-05-29 20:26:10 -0500

    Fix stale Android instrumented tests

--- app/src/androidTest/java/com/cicloguia/app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/cicloguia/app/ExampleInstrumentedTest.kt
@@ -3,22 +3,16 @@ package com.cicloguia.app
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
+    fun applicationId_matchesCurrentAppId() {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.bicivia.app", appContext.packageName)
+
+        assertEquals("com.cicloguia.app", appContext.packageName)
     }
 }
